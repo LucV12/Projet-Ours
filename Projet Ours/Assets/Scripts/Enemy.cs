0ef7c1d2a672512bfdd9f23cf56ee5c306802f73
LucV12 <l.vidal@rubika-edu.com>
2019-10-13 17:34:26 +0200

    Chargement de la Rage par la mort des ennemis

--- Projet Ours/Assets/Scripts/Enemy.cs
+++ Projet Ours/Assets/Scripts/Enemy.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public int health;
+    public bool executed;
+
+    public GameObject nounours;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        executed = false;
     }
 
     // Update is called once per frame
@@ -17,6 +20,15 @@ public class Enemy : MonoBehaviour
     {
         if(health <= 0)
         {
+            if (executed == false)
+            {
+                nounours.GetComponent<PlayerController>().rage += 0.1f;
+            }
+            else if (executed == true)
+            {
+                nounours.GetComponent<PlayerController>().rage += 0.5f;
+            }
+
             Destroy(gameObject);
         }
     }
