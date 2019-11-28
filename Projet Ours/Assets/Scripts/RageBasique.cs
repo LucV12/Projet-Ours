5ee5843a7ca4319a06586fb60c98f8e545b6a9b3
Luc <l.vidal@rubika-edu.com>
2019-11-28 12:01:11 +0100

    Rages et Ajout de l'explosif au Grabbable

---
+++ Projet Ours/Assets/Scripts/RageBasique.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RageBasique : MonoBehaviour
+{
+    public float rageTime;
+    public int rageDamageBoost;
+
+    public IEnumerator RageActive1()
+    {
+        StartCoroutine(GetComponent<PlayerController>().ColorChangeRage());
+        GetComponent<PlayerController>().damage = GetComponent<PlayerController>().damage * rageDamageBoost;
+        GetComponent<PlayerController>().canLooseLife = false;
+        Debug.Log("Rage Activée !!!");
+        yield return new WaitForSeconds(rageTime);
+        GetComponent<PlayerController>().damage = GetComponent<PlayerController>().damage / rageDamageBoost;
+        GetComponent<PlayerController>().canLooseLife = true;
+    }
+}
---
+++ Projet Ours/Assets/Scripts/RageTerrifiante.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RageTerrifiante : MonoBehaviour
+{
+    Transform terrorPos;
+    public float terrorRange;
+    public LayerMask whatIsEnemies;
+    public float terrorSpeed;
+    public float terrorTime;
+    GameObject nounours;
+
+    public IEnumerator RageActive3 ()
+    {
+        nounours = GameObject.FindGameObjectWithTag("Player");
+        terrorPos = nounours.transform;
+
+        StartCoroutine(GetComponent<PlayerController>().ColorChangeRage());
+
+        Collider2D[] enemyToTerrify = Physics2D.OverlapCircleAll(terrorPos.position, terrorRange, whatIsEnemies);
+        for (int i = 0; i < enemyToTerrify.Length; i++)
+        {
+            enemyToTerrify[i].GetComponent<Enemy>().repulsed = true;
+            enemyToTerrify[i].GetComponent<Enemy>().enemy.velocity = (enemyToTerrify[i].GetComponent<Enemy>().transform.position - GetComponent<PlayerController>().transform.position).normalized * terrorSpeed;
+        }
+        yield return new WaitForSeconds(terrorTime);
+        for (int i = 0; i < enemyToTerrify.Length; i++)
+        {
+            enemyToTerrify[i].GetComponent<Enemy>().repulsed = false;
+            enemyToTerrify[i].GetComponent<Enemy>().enemy.velocity = Vector2.zero;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(GetComponent<PlayerController>().transform.position, terrorRange);
+    }
+}
