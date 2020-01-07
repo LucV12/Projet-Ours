02235251fdb8156624a95875ba2a517984bf32bf
Côme Richetta <c.richetta@rubika-edu.com>
2020-01-07 18:10:20 +0100

    scripts OBJ + boss door

---
+++ Projet Ours/Assets/Scripts/OBJ/MachineOBJ.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineOBJ : MonoBehaviour
+{
+    public float HP;
+    public bool Destroyed1 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HP <= 0)
+        {
+            Destroyed1 = true;
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        HP -= damage;
+        Debug.Log("damage TAKEN !");
+    }
+}
---
+++ Projet Ours/Assets/Scripts/OBJ/ReservoirOBJ.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReservoirOBJ : MonoBehaviour
+{
+    public float HP;
+    public bool Destroyed2 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HP <= 0)
+        {
+            Destroyed2 = true;
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        HP -= damage;
+        Debug.Log("damage TAKEN !");
+    }
+}
